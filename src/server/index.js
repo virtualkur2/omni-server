0eaa17dabb0cdaef10b937b0a417a7361110acc6
Mauricio Contreras Canepa <virtual.kur2@gmail.com>
2019-03-06 01:26:16 +0100

    added morgan logger

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,12 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import morgan from 'morgan';
 //import path from 'path';
 import config from '../config';
 import routes from './routes';
 import errorHandler from './helpers/error-handler.helper';
+
 
 const router = express.Router();
 const server = () => {
@@ -16,6 +18,7 @@ const server = () => {
     extended: true
   }));
   app.use(cookieParser(config.cookieSecret));
+  config.env == 'development'? app.use(morgan('dev')) : app.use(morgan('combined'));
   app.use(routes(router));
   app.use(errorHandler);
   return app;
