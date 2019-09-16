39a8ad68d01938bb182e5701b14ff77d54078d1c
Mauricio Contreras <virtual.kur2@gmail.com>
2019-09-16 13:54:06 +0200

    Removed Domain Module (deprecated)

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
 import consoleStamp from 'console-stamp';
-import * as Domain from 'domain';
 
 import config from './config';
 import server from './server/';
 import dbHelper from './server/helpers/db-connection.helper';
 
-const d = Domain.create();
-d.run(() => {
-  const app = server();
-  console.info('Starting app...');
-  dbHelper.connect()
-    .then(() => {
-      console.info('===> Starting server...');
-      app.listen(config.port, (err) => {
-        if(err) {
-          console.error(err.message);
-          process.exit(1);
-        }
-        console.info(`===> Server succesfully started and listening on port ${config.port}.`);
-      });
-    })
-    .catch((err) => {
-      console.error('Error: ', err);
+const app = server();
+console.info('Starting app...');
+dbHelper.connect()
+.then(() => {
+  console.info('===> Starting server...');
+  app.listen(config.port, (err) => {
+    if(err) {
+      console.error(err.message);
       process.exit(1);
-    });
-});
-
-d.on('error', (err) => {
-  // TODO: Call error handler function for domain
-  console.error('Error in app. Cause: ', err.message);
-  console.error('A restart is needed.');
+    }
+    console.info(`===> Server succesfully started and listening on port ${config.port}.`);
+  });
+})
+.catch((err) => {
+  console.error('Error: ', err);
   process.exit(1);
 });
 
